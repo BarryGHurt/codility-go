a1eb810f0b2089b90f3dfe03f327f87aeb3de2fc
BarryGHurt <barry.hurt@protonmail.com>
2020-11-30 15:19:42 -0800

    mansplained it to myself

--- codility.go
+++ codility.go
@@ -1,5 +1,10 @@
 package codility
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // BinaryGap - find the binary gap of an int
 func BinaryGap(N int) int {
 	var result int
@@ -7,16 +12,22 @@ func BinaryGap(N int) int {
 	var calc bool
 
 	for N > 0 {
+		binrep := strconv.FormatInt(int64(N), 2)
+		fmt.Println(binrep)
+
 		// N is odd
 		if N%2 == 1 {
+			// Don't count till the first '1' is seen
 			if !calc {
 				calc = true
+				// Reset count every '1' thereafter
 			} else {
 				if resultTemp > result {
 					result = resultTemp
 				}
 				resultTemp = 0
 			}
+			// N is even, thus a '0' at the end in binary
 		} else {
 			if calc {
 				resultTemp++
